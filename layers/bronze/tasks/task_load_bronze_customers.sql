a541f5bd2657a0269f13a004fcc3fe69cbae74c7
Marcelo Ribas <marcelo.pachione@icloud.com>
2025-12-16 11:05:56 +0000

    add layer silver - ddl, sp and task

--- layers/bronze/tasks/task_load_bronze_customers.sql
+++ layers/bronze/tasks/task_load_bronze_customers.sql
@@ -2,14 +2,17 @@ USE DATABASE snowDB;
 USE SCHEMA DEV;
 
 -- Task Bronze
-CREATE OR REPLACE TASK task_bronze_customers
+CREATE OR REPLACE TASK task_load_bronze_customers
   WAREHOUSE = COMPUTE_WH
   SCHEDULE = 'USING CRON */5 * * * * Europe/Dublin' -- every 5 minutes
 AS
   CALL load_bronze_customers();
 
--- Enalble the task
--- ALTER TASK task_bronze_customers RESUME;
+-- Enable the task
+-- ALTER TASK task_load_bronze_customers RESUME;
 
 -- List tasks execution history
--- SHOW TASKS LIKE 'TASK_BRONZE_CUSTOMERS';
+-- SHOW TASKS LIKE 'task_load_bronze_customers';
+
+-- Drop task
+-- DROP TASK TASK_BRONZE_CUSTOMERS;
---
+++ layers/silver/tasks/task_load_silver_customers.sql
@@ -0,0 +1,16 @@
+USE DATABASE snowDB;
+USE SCHEMA DEV;
+
+-- Task Bronze
+CREATE OR REPLACE TASK task_load_silver_customers
+  WAREHOUSE = COMPUTE_WH
+  after task_load_bronze_customers
+AS 
+    call task_load_silver_customers()
+;
+
+-- Enable the task
+-- ALTER TASK task_load_silver_customers RESUME;
+
+-- List tasks execution history
+-- SHOW TASKS LIKE 'task_load_silver_customers';
