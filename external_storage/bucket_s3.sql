e43a9524d1af9e510b3c1de4c78fb9c95126a2be
Marcelo Ribas <marcelo.pachione@icloud.com>
2025-12-16 14:04:34 +0000

    fix: command to access s3 bucket

--- external_storage/bucket_s3.sql
+++ external_storage/bucket_s3.sql
@@ -2,11 +2,11 @@ USE DATABASE snowDB;
 USE SCHEMA PUBLIC;
 
 -- Create stage for S3 bucket North
-CREATE STAGE North 
+CREATE STAGE IF NOT EXISTS North 
 	URL = 's3://snowflake-arruda-s3/north/' 
 	CREDENTIALS = ( AWS_KEY_ID = '**********' AWS_SECRET_KEY = '*******' ) 
 	DIRECTORY = ( ENABLE = true ) 
 	COMMENT = 'Project - end2end-snowflake-sql-cicd';
 
 -- List files in the stage
-LIST @North;
+-- LIST @North;
