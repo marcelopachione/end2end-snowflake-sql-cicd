273edd00db8338f62e390201de17e00038a37a13
Marcelo Ribas <marcelo.pachione@icloud.com>
2025-12-16 10:28:15 +0000

    add database script, file_format script and external storage script

---
+++ layers/bronze/ddl/bronze_customers.sql
@@ -0,0 +1,9 @@
+USE DATABASE snowDB;
+USE SCHEMA DEV
+
+-- Create bronze table customer
+CREATE TABLE IF NOT EXISTS bronze_customers (
+    raw VARIANT,           -- Raw JSON
+    filename STRING,       -- Name file
+    created_at TIMESTAMP   -- Timestamp of load
+);
