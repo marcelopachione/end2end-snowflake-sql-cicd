58a7b491e4c436f70032bff3ec5dabf23f425cf9
Marcelo Ribas <marcelo.pachione@icloud.com>
2025-12-17 17:58:52 +0000

    feat: rebuild proejct

--- migrations/snowflake/001_bucket_s3.sql
+++ migrations/snowflake/001_bucket_s3.sql
@@ -1,15 +1,13 @@
 -- migrations/snowflake/001_bucket_s3.sql
--- !set variable_substitution=true  -- habilita &{VAR} em execução local também
+!set variable_substitution=true
 
 USE SCHEMA PUBLIC;
 
 CREATE STAGE IF NOT EXISTS North
   URL = 's3://snowflake-arruda-s3/north/'
   CREDENTIALS = (
-    -- AWS_KEY_ID     = '&{SNOWSQL_AWS_KEY_ID}'        -- Nomes alinhados aos -D do CI
-    -- AWS_SECRET_KEY = '&{SNOWSQL_AWS_SECRET_KEY}'
-    AWS_KEY_ID     = '$SNOWSQL_AWS_KEY_ID'        -- Nomes alinhados aos -D do CI
-    AWS_SECRET_KEY = '$SNOWSQL_AWS_SECRET_KEY'
+    AWS_KEY_ID     = '&{SNOWSQL_AWS_KEY_ID}'
+    AWS_SECRET_KEY = '&{SNOWSQL_AWS_SECRET_KEY}'
   )
   DIRECTORY = (ENABLE = TRUE)
   COMMENT = 'Project - end2end-snowflake-sql-cicd';
