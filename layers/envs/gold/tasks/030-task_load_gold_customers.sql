87652651ae218552dbd2620e64ad9a1756439e79
Marcelo Ribas <marcelo.pachione@icloud.com>
2025-12-18 12:39:38 +0000

    feat: add view VW_BRONZE_CUSTOMERS

--- layers/envs/gold/tasks/030-task_load_gold_customers.sql
+++ layers/envs/gold/tasks/030-task_load_gold_customers.sql
@@ -3,8 +3,7 @@ CREATE OR REPLACE TASK task_load_gold_customers
   WAREHOUSE = COMPUTE_WH
   after task_load_silver_customers
 AS 
-    call sp_load_gold_dim_customers()
-;
+    call sp_load_gold_dim_customers();
 
 -- Enable the task
 -- ALTER TASK task_load_gold_customers RESUME;
